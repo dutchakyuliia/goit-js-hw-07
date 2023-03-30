fcce9092d1eccb4e818ef5965092cc220ca9d423
Dutchak Yuliia <dutchakyuliia@gmail.com>
2023-03-30 13:57:56 +0200

    add and remove folder

---
+++ js/01-gallery.js
@@ -0,0 +1,14 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+// const markup = galleryItems
+//   .map((technology) => `<li class="list-item">${technology}</li>`)
+//   .join("");
+
+// console.log(markup);
+
+// galleryList.innerHTML = markup;
