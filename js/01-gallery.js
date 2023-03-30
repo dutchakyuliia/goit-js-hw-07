6235155c1a8895ed62037453a5b1e9fcd5a716f2
Dutchak Yuliia <dutchakyuliia@gmail.com>
2023-03-30 18:28:26 +0200

    make first gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
 const galleryList = document.querySelector(".gallery");
 
-// const markup = galleryItems
-//   .map((technology) => `<li class="list-item">${technology}</li>`)
-//   .join("");
+const markup = galleryItems
+  .map(
+    (item) =>
+      `<li class="gallery__item">
+  <a onclick="event.preventDefault()" class="gallery__link" href="large-image.jpg">
+    <img 
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
 
-// console.log(markup);
+console.log(markup);
+galleryList.innerHTML = markup;
 
-// galleryList.innerHTML = markup;
+galleryList.addEventListener("click", (event) => {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${event.target.getAttribute(
+      "data-source"
+    )}" width="800" height="600">
+`);
+  instance.show();
+});
