50139b28f77a31a7417b40809779faa2d6d72a30
Dutchak Yuliia <dutchakyuliia@gmail.com>
2023-03-31 12:01:31 +0200

    resolve task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (item) =>
+      `<li class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img class="gallery__image" src="${item.preview}" alt="${item.description}" title="${item.description}" />
+            </a>
+        </li>`
+  )
+  .join("");
+
+console.log(markup);
+galleryList.innerHTML = markup;
+
+let lightbox = new SimpleLightbox(".gallery a", {});
